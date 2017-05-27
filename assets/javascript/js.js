6c9686e3ab7deb3263b1b920ee25fa6fd0cd1edb
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-05-27 01:40:19 -0400

    adding questions

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -20,12 +20,37 @@ function changeFriend(){
 	$("#demonPic").css("right", distance+"%");
 	setTimeout(moveEnemy, 1000);
 }
+
 function nextQuestion(){
 
 }
+
+function Question(txt,answers,correctIndex) {
+	this.question = txt;
+	this.answers = answers;
+	this.correctIndex = correctIndex;
+}
+
+function game() {
+	//holds the question objects.
+	this.questions = [];
+	for (var i = 0; i <10;i++){
+		this.questions[i] = new Question(libQestions[i],libAnswers[i],libIndecies[i]);
+	}
+}
+
+
 var globalTime = 10
 var timeRemaining = globalTime;
 var currentFriend = 0;
 var friendArray = ["cordelia","xander","oz","willow","giles"];
+
 $("#friendPic").attr("src","assets/images/" + friendArray[currentFriend] + ".png");
-//setTimeout(moveEnemy, 1000);
+
+$("#startGame").on("click",function () {
+	$("#intro").hide();
+	//load random question
+	//start Timer
+	//setTimeout(moveEnemy, 1000);
+});
+
---
+++ assets/javascript/questionLibrary.js
@@ -0,0 +1,37 @@
+var libQuestions[
+	"How does a Chinese Vamipre get around?",
+	"Which vegetable is a Vampire's Achilles' heel?",
+	"What actor is most known for his iconic portrayal of dracula?",
+	"Who wrote the original book 'Dracula'?",
+	"Which of the following is NOT a method used to kill a Vampire?",
+	"Which real-life person is the character of Dracula thought to be based on?",
+	"What is a sure-fire way to stop a pursuant Chinese vampire?",
+	"",
+	"",
+	""
+	];
+var libAnswers[
+	["He flies.","He hops like a rabbit.","He cannot move on his own; he must inhabit another's body.","He crawls on all fours."],
+	["Beets.","Potatoes.","Onions.","Garlic."],
+	["Bela Lugosi","Keanu Reeves","Robert Pattinson","Anthony Hopkins"],
+	["Mary Shelly","Ed Wood","Bram Stoker","Charles Dickens"],
+	["Put a stake through its heart.","Remove its head.","Drown it in water.","Burn it with fire."],
+	["Vlad the Impaler.","Count Bathory.","Hannibal of Carthage.","Van Helsing."],
+	["Punch it in the head.","Throw rice on the ground.","Place a bucket of water in its path.","Shout its name."],
+	["","","",""],
+	["","","",""],
+	["","","",""]
+	];
+
+var libIndecies[
+	1,
+	3,
+	0,
+	2,
+	2,
+	0,
+	1,
+	,
+	,
+	
+	];
