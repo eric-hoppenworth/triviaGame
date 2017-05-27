4675423c5804f435586f1a96516f0c08bd07183a
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-05-26 23:31:02 -0400

    all character sprites added

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,14 +1,31 @@
 function moveEnemy(){
 	timeRemaining--;
-	var distance = 90*timeRemaining/20;
+	var distance = 90*timeRemaining/globalTime;
 	$("#demonPic").css("right", distance+"%");
 	if (timeRemaining > 0){
 		setTimeout(moveEnemy, 1000);	
 	} else{
 		//out of time!
 		setTimeout(alert, 10,"buzz!");
+		setTimeout(changeFriend,11);
 	}
 	
 }
-var timeRemaining = 20;
+
+function changeFriend(){
+	currentFriend ++;
+	$("#friendPic").attr("src","assets/images/" + friendArray[currentFriend] + ".png");
+	timeRemaining = globalTime
+	var distance = 90*timeRemaining/globalTime;
+	$("#demonPic").css("right", distance+"%");
+	setTimeout(moveEnemy, 1000);
+}
+function nextQuestion(){
+
+}
+var globalTime = 10
+var timeRemaining = globalTime;
+var currentFriend = 0;
+var friendArray = ["cordelia","xander","oz","willow","giles"];
+$("#friendPic").attr("src","assets/images/" + friendArray[currentFriend] + ".png");
 //setTimeout(moveEnemy, 1000);
