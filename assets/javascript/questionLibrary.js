b60ee53812a16199baaf12cb6c7939036dfc233d
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-05-27 20:35:03 -0400

    intro bootstrapped

--- assets/javascript/questionLibrary.js
+++ assets/javascript/questionLibrary.js
@@ -1,4 +1,4 @@
-var libQuestions[
+var libQuestions =[
 	"How does a Chinese Vamipre get around?",
 	"Which vegetable is a Vampire's Achilles' heel?",
 	"What actor is most known for his iconic portrayal of dracula?",
@@ -6,11 +6,11 @@ var libQuestions[
 	"Which of the following is NOT a method used to kill a Vampire?",
 	"Which real-life person is the character of Dracula thought to be based on?",
 	"What is a sure-fire way to stop a pursuant Chinese vampire?",
-	"",
-	"",
+	"From which source did the character Blade originate?",
+	"Which of the following is NOT a common trait of a vamipre?",
 	""
 	];
-var libAnswers[
+var libAnswers =[
 	["He flies.","He hops like a rabbit.","He cannot move on his own; he must inhabit another's body.","He crawls on all fours."],
 	["Beets.","Potatoes.","Onions.","Garlic."],
 	["Bela Lugosi","Keanu Reeves","Robert Pattinson","Anthony Hopkins"],
@@ -18,12 +18,12 @@ var libAnswers[
 	["Put a stake through its heart.","Remove its head.","Drown it in water.","Burn it with fire."],
 	["Vlad the Impaler.","Count Bathory.","Hannibal of Carthage.","Van Helsing."],
 	["Punch it in the head.","Throw rice on the ground.","Place a bucket of water in its path.","Shout its name."],
-	["","","",""],
-	["","","",""],
+	["The movie 'Blade'.","The X-men comics","Spiderman Comics","The 'Batman' TV show."],
+	["He has no reflection.","Sunlight will hurt him.","He cannot touch a cross.","He is not able to cross running water."],
 	["","","",""]
 	];
 
-var libIndecies[
+var libIndecies =[
 	1,
 	3,
 	0,
@@ -31,7 +31,7 @@ var libIndecies[
 	2,
 	0,
 	1,
-	,
-	,
+	2,
+	3,
 	
 	];
