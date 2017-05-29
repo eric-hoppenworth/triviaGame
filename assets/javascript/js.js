190ebd2f1e89180099366a2130aa6dfc1cb5728c
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-05-29 18:18:18 -0400

    responsiveness complete

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -18,6 +18,9 @@ $(document).ready(function(){
 
 	function changeFriend(){
 		currentFriend ++;
+		if (currentFriend === friendArray.length){
+			return true;
+		}
 		$("#friendPic").attr("src","assets/images/" + friendArray[currentFriend] + ".png");
 		timeRemaining = globalTime
 		$("#demonPic").css("left", "0vw");
@@ -25,13 +28,15 @@ $(document).ready(function(){
 	}
 
 
-
+	//question Object constuctor
 	function Question(txt,answers,correctIndex) {
 		this.question = txt;
 		this.answers = answers;
 		this.correctIndex = correctIndex;
 	}
 
+
+	//game object constructor
 	function game() {
 		//holds the question objects.
 		this.questions = [];
@@ -41,16 +46,38 @@ $(document).ready(function(){
 		for (var i = 0; i <10;i++){
 			this.questions[i] = new Question(libQuestions[i],libAnswers[i],libIndecies[i]);
 		}
-
+		this.bulbCounts = [0,0];
+		this.marqueeHeights = [0,0];
+		this.orientation = 0;
+		this.bothOrientSet = false;
+		
 
 		this.nextQuestion = function(){
 			this.currQuestion ++;
 			var self = this;
-			$("#questionText").html(self.questions[self.currQuestion].question)
+			$("#questionText").html(self.questions[self.currQuestion].question);
+
 			$(".option").each(function(i){
 				$(this).html(self.questions[self.currQuestion].answers[i]);
 			});
+
 			myGame.createBulbs();
+		}
+
+		this.setBulbs = function(index){
+			//initializes the bulb count for that specific oriention.\
+			var marqueeHeight = parseInt($("#marquee").css("height"));
+			var singleBulbHeight = parseFloat($(".col-xs-1").css("width"));
+			if (singleBulbHeight> 50){
+				singleBulbHeight = 50;
+			}
+
+			var bulbCount = Math.ceil(marqueeHeight/singleBulbHeight);
+
+			this.bulbCounts[index] = bulbCount;
+			this.marqueeHeights[index] = singleBulbHeight*bulbCount; 
+			$("#marquee").css("height",this.marqueeHeights[index]+"px");
+
 		}
 
 		this.createBulbs= function(){
@@ -60,17 +87,36 @@ $(document).ready(function(){
 			if (singleBulbHeight> 50){
 				singleBulbHeight = 50;
 			}
-			//var bulbsHeight = parseInt($("#bulbs").css("height"));
-
-			var counter = 0;
 			var bulbCount = Math.ceil(marqueeHeight/singleBulbHeight);
-			$("#marquee").css("height",(singleBulbHeight*bulbCount)+"px");
 
+			var makeTheBulbs = false;
+
+			if (this.bulbCounts[this.oriention]===bulbCount){
+				//do nothing
+				makeTheBulbs = false;
+			} else{
+				if (this.bothOrientSet === false){
+					//set the new oriention
+					this.setBulbs(1);
+					this.bothOrientSet = true;
+					makeTheBulbs = true;
+				} else{
+					if(this.oriention === 0){
+						this.oriention = 1;
+						makeTheBulbs = true;
+					}else{
+						this.oriention = 0;
+						makeTheBulbs = true;
+					}
+				}
+			}
+
+			//escape if I do not need to make any bulbs
+			if (makeTheBulbs === false){return false;}
 			
-			// if(bulbsHeight - singleBulbHeight < marqueeHeight && marqueeHeight < bulbsHeight + singleBulbHeight){
-			// 	//then I am within 1 bulb of being perfext, so don't adjust.
-			// 	return false
-			// }
+			var counter = 0;
+			bulbCount = this.bulbCounts[this.oriention];
+			$("#marquee").css("height",this.marqueeHeights[this.oriention]+"px");
 
 			//first, clear out any existing bulbs
 			$("#bulbTopRow").empty();
@@ -113,19 +159,20 @@ $(document).ready(function(){
 		}
 	}
 
-	var globalTime = 10;
+	var globalTime = 30;
 	var timeRemaining = globalTime;
 	var currentFriend = 0;
 	var friendArray = ["cordelia","xander","oz","willow","giles"];
+
+	//orientation holds the current orientation of the device.  either 0 or 1, but there is no specific orientation for the number
+
 	myGame = new game();
 	
-	//setTimeout(moveEnemy, 1000);
-
-
 	$("#friendPic").attr("src","assets/images/" + friendArray[currentFriend] + ".png");
 
 	$("#startGame").on("click",function () {
 		$("#intro").hide();
+		myGame.setBulbs(myGame.orientation);
 		myGame.createBulbs();
 		//load random question
 		//start Timer
